df7265c4decd9d916a00e387a737b087069fd5fd
Francesca Sophia De Guzman <hiamphia@gmail.com>
2024-11-14 14:58:23 +0800

    Added show/hide password on reset_password page

--- php/includes/sidebar_faculty.php
+++ php/includes/sidebar_faculty.php
@@ -42,31 +42,31 @@ $isCPTActive = in_array($activePage, ['CPT', 'CPT_Request', 'CPT_Teaching', 'CPT
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item ms-3">
-                            <a href="/careerpath/php/dashboard/career_progress_tracking/career_progress_request.php" class="nav-link <?php echo ($activePage == 'CPT_Request') ? 'active' : ''; ?>">
+                            <a href="php/dashboard/career_progress_tracking/career_progress_request.php" class="nav-link <?php echo ($activePage == 'CPT_Request') ? 'active' : ''; ?>">
                                 <i class="nav-icon bi bi-file-earmark-text"></i> 
                                 <p>Request Form</p>
                             </a>
                         </li>
                         <li class="nav-item ms-3">
-                            <a href="/careerpath/php/dashboard/career_progress_tracking/career_progress_teaching.php" class="nav-link <?php echo ($activePage == 'CPT_Teaching') ? 'active' : ''; ?>">
+                            <a href="php/dashboard/career_progress_tracking/career_progress_teaching.php" class="nav-link <?php echo ($activePage == 'CPT_Teaching') ? 'active' : ''; ?>">
                                 <i class="nav-icon bi bi-journal-text"></i> 
                                 <p>Teaching</p>
                             </a>
                         </li>
                         <li class="nav-item ms-3">
-                            <a href="/careerpath/php/dashboard/career_progress_tracking/career_progress_research.php" class="nav-link <?php echo ($activePage == 'CPT_Research') ? 'active' : ''; ?>">
+                            <a href="php/dashboard/career_progress_tracking/career_progress_research.php" class="nav-link <?php echo ($activePage == 'CPT_Research') ? 'active' : ''; ?>">
                                 <i class="nav-icon bi bi-journal-medical"></i> 
                                 <p>Research, Innovation, and Creative Works</p>
                             </a>
                         </li>
                         <li class="nav-item ms-3">
-                            <a href="/careerpath/php/dashboard/career_progress_tracking/career_progress_extension.php" class="nav-link <?php echo ($activePage == 'CPT_Extension') ? 'active' : ''; ?>">
+                            <a href="php/dashboard/career_progress_tracking/career_progress_extension.php" class="nav-link <?php echo ($activePage == 'CPT_Extension') ? 'active' : ''; ?>">
                                 <i class="nav-icon bi bi-people"></i>
                                 <p>Extension Services</p>
                             </a>
                         </li>
                         <li class="nav-item ms-3">
-                            <a href="/careerpath/php/dashboard/career_progress_tracking/career_progress_professional.php" class="nav-link <?php echo ($activePage == 'CPT_Professional') ? 'active' : ''; ?>">
+                            <a href="php/dashboard/career_progress_tracking/career_progress_professional.php" class="nav-link <?php echo ($activePage == 'CPT_Professional') ? 'active' : ''; ?>">
                                 <i class="nav-icon bi bi-award"></i> 
                                 <p>Professional Development</p>
                             </a>
@@ -74,18 +74,18 @@ $isCPTActive = in_array($activePage, ['CPT', 'CPT_Request', 'CPT_Teaching', 'CPT
                     </ul>
                 </li>
 
-                <li class="nav-item"> <a href="/careerpath/php/dashboard/performance_summary_report.php" class="nav-link <?php echo ($activePage == 'PSR') ? 'active' : ''; ?>"> <i class="nav-icon bi bi-bar-chart"></i>
+                <li class="nav-item"> <a href="php/dashboard/performance_summary_report.php" class="nav-link <?php echo ($activePage == 'PSR') ? 'active' : ''; ?>"> <i class="nav-icon bi bi-bar-chart"></i>
                         <p><strong>Performance Summary Reports</strong>
                         </p>
                     </a>
                 </li>
                 
-                <li class="nav-item"> <a href="/careerpath/php/dashboard/document_management.php" class="nav-link <?php echo ($activePage == 'Documents') ? 'active' : ''; ?>"> <i class="nav-icon bi bi-folder"></i>
+                <li class="nav-item"> <a href="php/dashboard/document_management.php" class="nav-link <?php echo ($activePage == 'Documents') ? 'active' : ''; ?>"> <i class="nav-icon bi bi-folder"></i>
                         <p><strong>Document Management</strong></p>
                     </a>
                 </li>
 
-                <li class="nav-item"> <a href="/careerpath/php/dashboard/notifications.php" class="nav-link <?php echo ($activePage == 'Notifs') ? 'active' : ''; ?>"> <i class="nav-icon bi bi-bell"></i>
+                <li class="nav-item"> <a href="php/dashboard/notifications.php" class="nav-link <?php echo ($activePage == 'Notifs') ? 'active' : ''; ?>"> <i class="nav-icon bi bi-bell"></i>
                         <p><strong>Notifications</strong>
                         </p>
                     </a>
@@ -102,13 +102,13 @@ $isCPTActive = in_array($activePage, ['CPT', 'CPT_Request', 'CPT_Teaching', 'CPT
                     </ul> -->
                 </li>
 
-                <li class="nav-item"> <a href="/careerpath/php/dashboard/opportunities.php" class="nav-link <?php echo ($activePage == 'Opportunities') ? 'active' : ''; ?>"> <i class="nav-icon bi bi-briefcase"></i>
+                <li class="nav-item"> <a href="php/dashboard/opportunities.php" class="nav-link <?php echo ($activePage == 'Opportunities') ? 'active' : ''; ?>"> <i class="nav-icon bi bi-briefcase"></i>
                         <p><strong>Job/Promotion Opportunities</strong>
                         </p>
                     </a>
                 </li>
 
-                <li class="nav-item"> <a href="/careerpath/php/dashboard/resources_training.php" class="nav-link <?php echo ($activePage == 'Training') ? 'active' : ''; ?>"> <i class="nav-icon bi bi-book"></i>
+                <li class="nav-item"> <a href="php/dashboard/resources_training.php" class="nav-link <?php echo ($activePage == 'Training') ? 'active' : ''; ?>"> <i class="nav-icon bi bi-book"></i>
                         <p><strong>Resources and Training</strong></p>
                     </a>
                 </li>
@@ -139,7 +139,7 @@ $isCPTActive = in_array($activePage, ['CPT', 'CPT_Request', 'CPT_Teaching', 'CPT
                         </li>
                     </ul>
                 </li>
-                <li class="nav-item"> <a href="/careerpath/php/logout.php" class="nav-link"> <i class="nav-icon bi bi-box-arrow-right"></i>
+                <li class="nav-item"> <a href="php/logout.php" class="nav-link"> <i class="nav-icon bi bi-box-arrow-right"></i>
                         <p><strong>Sign Out</strong></p> </a>
                 </li>
             </ul>
