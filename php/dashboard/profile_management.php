4260890572e325771a1149356e2b3e05aa7dcf5b
Francesca Sophia De Guzman <hiamphia@gmail.com>
2024-11-26 03:20:17 +0800

    Removed redundant JS link in Profile Management page

--- php/dashboard/profile_management.php
+++ php/dashboard/profile_management.php
@@ -522,7 +522,6 @@ if (isset($_GET['request'])) {
     <!-- Cropper.js JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.js"></script>
     <!-- Bootstrap JS Bundle (includes Popper) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 
     <!-- JavaScript for Cropping and Uploading -->
