1a0f11d5241ab8a6c4c6780412d8b5ad69356d8a
Francesca Sophia De Guzman <hiamphia@gmail.com>
2025-01-07 16:23:48 +0800

    KRA 1 Upload Evidence Modal button change from 'Delete' to 'Remove'

--- php/includes/career_progress_tracking/teaching/modals.php
+++ php/includes/career_progress_tracking/teaching/modals.php
@@ -77,7 +77,7 @@
                         <label for="secondSemesterFile" class="form-label">2nd Semester:</label>
                         <input type="file" class="form-control" id="secondSemesterFile" name="fileSecondSemester" accept=".pdf, .doc, .docx, .jpg, .jpeg, .png, .xlsx, .xls">
                         <div id="secondSemesterFilename" class="mt-2"></div>
-                        <button type="button" class="btn btn-sm btn-danger mt-2" id="deleteFile2" data-semester="sem2">Delete</button>
+                        <button type="button" class="btn btn-sm btn-danger mt-2" id="deleteFile2" data-semester="sem2">Remove</button>
                     </div>
                     <input type="hidden" id="modal_request_id" name="request_id">
                     <input type="hidden" id="modal_evaluation_id" name="evaluation_id">
